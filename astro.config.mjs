0f50d202c1278a46cfabd9f5289efa51df175359
marcello-tk <awesomedevguynull@gmail.com>
2026-06-16 20:56:51 +0200

    feat: add custom fonts, extend design tokens

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  fonts: [{
+    provider: fontProviders.google(),
+    name: "Lexend Deca",
+    cssVariable: "--ff-sans",
+    styles: ["normal"],
+    weights: ["200 900"],
+    fallbacks: ["sans-serif"]
+  }]
 });
